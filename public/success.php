86548c742d11aef67f70b28cc08020f317af44a6
DawnBaker <deltabravo73@gmail.ca>
2020-04-28 13:23:05 -0500

    Assignment 2 - updated Success.php to look like other pages, completed styling of information in header.inc.php file with if statement in php

--- public/success.php
+++ public/success.php
@@ -4,7 +4,11 @@
  * Intro PHP             
  * Assignment 2          
  */
-require __DIR__ . '/../config.php';
+
+$title = "Registration Success!";
+
+require __DIR__ . '/../includes/header.inc.php';
+
 
 if(empty($_GET['user_id'])) {
     die('Please use form to register');
@@ -23,27 +27,27 @@ $stmt->execute($params);
 
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Success</title>
-</head>
-<body>
-    <h1>Thank you for registering with us!</h1>
+
+
+?>
+    <h2>Thank you for registering with us!</h2>
 
     <p>You have entered the following information:</p>
+    <div class="user_info">
+        <ul>
+            <li><strong>First Name :</strong>&nbsp;&nbsp; <?=$result['first_name']?></li>
+            <li><strong>Last Name :</strong>&nbsp;&nbsp; <?=$result['last_name']?></li>
+            <li><strong>Email :</strong>&nbsp;&nbsp; <?=$result['email']?></li>
+            <li><strong>Phone Number :</strong>&nbsp;&nbsp; <?=$result['phone_num']?></li>
+            <li><strong>Address :</strong>&nbsp;&nbsp; <?=$result['address']?></li>
+            <li><strong>City :</strong>&nbsp;&nbsp; <?=$result['city']?></li>
+            <li><strong>Province :</strong>&nbsp;&nbsp; <?=$result['prov']?></li>
+            <li><strong>Postal Code :</strong>&nbsp;&nbsp; <?=$result['post_code']?></li>
+            <li><strong>Country :</strong>&nbsp;&nbsp; <?=$result['country']?></li>
+        </ul>
+    </div>
+<?php
 
-    <ul>
-        <li><strong>First Name:</strong><?=$result['first_name']?></li>
-        <li><strong>Last Name:</strong><?=$result['last_name']?></li>
-        <li><strong>Email:</strong><?=$result['email']?></li>
-        <li><strong>Phone Number:</strong><?=$result['phone_num']?></li>
-        <li><strong>Address:</strong><?=$result['address']?></li>
-        <li><strong>City:</strong><?=$result['city']?></li>
-        <li><strong>Province:</strong><?=$result['prov']?></li>
-        <li><strong>Postal Code:</strong><?=$result['post_code']?></li>
-        <li><strong>Country:</strong><?=$result['country']?></li>
-    </ul>
-</body>
-</html>
+require __DIR__ . '/../includes/footer.inc.php'
+
+?>
