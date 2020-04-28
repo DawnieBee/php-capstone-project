01b641978f10beced3bbddf1d7c9ec34d3527acf
DawnBaker <deltabravo73@gmail.ca>
2020-04-28 07:46:00 -0500

    added first and last name validators to make sure fields not empty and added if statement to confirm form data is a POST request

--- public/handle_form.php
+++ public/handle_form.php
@@ -12,6 +12,18 @@ require __DIR__ . '/../config.php';
 
 session_start();
 
+// confirm form data is a POST request or die 
+if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die('Unsupported request method.');
+}
 
 // empty $errors array
 $errors = [];
+
+// name validators 
+if(empty($_POST['first_name'])) {
+    $errors['first_name'] = 'First Name is a required field';
+} 
+if(empty($_POST['last_name'])) {
+    $errors['last_name'] = 'Last Name is a required field';
+} 
