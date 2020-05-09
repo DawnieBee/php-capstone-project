626b78ce5985abee2e98eb1748edfd75a5084cd1
DawnBaker <deltabravo73@gmail.ca>
2020-05-09 11:18:02 -0500

    assignment 2 - created if statement in nav includes to show if user session is set... allow to move through site

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -3,6 +3,7 @@
  * Inter PHP
  *Assignment 1
  -->
+
         <ul>
             <!-- breadcrumbs, active state based on title of the page -->
             <li class="curved_borders"><a <?=($title == "Honey...We're Home") ? 'class="current"' : ''?> href="index.php">Home</a></li>
@@ -10,11 +11,10 @@
             <li class="curved_borders"><a <?=($title == "Areas") ? 'class="current"' : ''?> href="areas.php">Areas</a></li>
             <li class="curved_borders"><a <?=($title == "Community Services") ? 'class="current"' : ''?> href="more.php">More</a></li>
             
-            <?php if(!empty($_SESSION['user_id'] = $user['user_id'])) : ?>
+            <?php if(!isset($_SESSION['user'])) : ?>      
             <!--  if not logged in show these  -->
                 <li class="curved_borders"><a <?=($title == "Profile") ? 'class="current"' : ''?> href="profile.php">Profile</a></li>
-                <li class="curved_borders"><a <?=($title == "Profile") ? 'class="current"' : ''?> href="profile.php">Logout</a></li>
-                
+                <li class="curved_borders"><a <?=($title == "Profile") ?> href="profile.php">Logout</a></li>
             <?php else: ?>
             <!-- if logged in show these -->
                 <li class="curved_borders"><a <?=($title == "Register With Us") ? 'class="current"' : ''?> href="register.php">Sign Up</a></li>
