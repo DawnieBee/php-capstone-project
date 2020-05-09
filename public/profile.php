d49fcf2c68a2e3a694b07d17086018735545bac0
DawnBaker <deltabravo73@gmail.ca>
2020-05-08 23:41:59 -0500

    added profile page and added the functions to verify whether credentials are valid and profile page if logged in.  also added logout button"

---
+++ public/profile.php
@@ -0,0 +1,75 @@
+<?php
+
+$title = "Profile";
+
+require __DIR__ . '/../includes/header.inc.php';
+
+if(empty($_SESSION['user_id'])) {
+    $errors['credentials'] = 'You must be logged in to see that page.';
+    $_SESSION['errors'] = $errors;
+    header('Location: login.php');
+    die;
+}
+
+$query = 'SELECT * 
+        FROM users 
+        WHERE user_id = :user_id';
+$stmt = $dbh->prepare($query);
+$params = array(
+    ':user_id' => $_SESSION['user_id']
+    );
+$stmt->execute($params);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+
+    <h2>Welcome back <?=esc($user['first_name'])?>! You have successfully logged in.</h2>
+
+    <p>Here is your user information:</p>
+    <div class="user_info">
+        <table>
+            <tr>
+                <th><strong>First Name</strong></th>
+                <td><?=$user['first_name']?></td>
+            </tr>
+            <tr>
+                <th><strong>Last Name</strong></th> 
+                <td><?=$user['last_name']?></td>
+            </tr>
+            <tr>
+                <th><strong>Email</strong></th>
+                <td><?=$user['email']?></td>
+            </tr>
+            <tr>
+                <th><strong>Phone Number</strong></th> 
+                <td><?=$user['phone_num']?></td>
+            </tr>
+            <tr>
+                <td><strong>Address</strong></td>
+                <td><?=$user['address']?></td>
+            </tr>
+            <tr>
+                <td><strong>City</strong></td>
+                <td><?=$user['city']?></td>
+            </tr>
+            <tr>
+                <td><strong>Province</strong></td>
+                <td><?=$user['prov']?></td>
+            </tr>
+            <tr>
+                <td><strong>Postal Code</strong></td>
+                <td><?=$user['post_code']?></td>
+            </tr>
+                <td><strong>Country</strong></td>
+                <td><?=$user['country']?></td>
+            </tr>
+        </table>
+    </div>
+
+    <p><a href="login.php?logout=1">Logout</a></p>
+<?php
+
+require __DIR__ . '/../includes/footer.inc.php'
+
+?>
