98be5d93ae0d8c6ff006f11922626ebcad3b6d7f
DawnBaker <deltabravo73@gmail.ca>
2020-05-18 15:40:19 -0500

    created admin folder and added a neighborhoods.php file in there for populating in a list all the neighborhoods info I want to display on the admin folder

--- public/admin/neighborhoods.php
+++ public/admin/neighborhoods.php
@@ -1,74 +1,98 @@
 <?php
 
-namespace Capstone;
+require __DIR__ . '/../../config.php';
 
-$neighborhood = new Model();
+$query = "SELECT * FROM neighborhoods";
+
+$stmt = $dbh->query($query);
+
+$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
 
 ?><!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-        <nav class="navbar navbar-inverse .navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Administration</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Dashboard</a></li>
-                    <li><a href="#">Neighborhoods</a></li>
-                    <li><a href="#">Users</a></li>
-                    <li><a href="#">Baskets</a></li>
-                </ul>
-                <!-- search field -->
-                <form class="navbar-form navbar-left" action="/action_page.php">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                        <div class="input-group-btn">
-                            <button class="btn btn-default" type="submit">
-                                <i class="glyphicon glyphicon-search"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span>Login</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Logout</a></li>
-                </ul>
-            </div>
-        </nav>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Neighborhoods | Admin</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="/admin/css/style.css" type="text/css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <a class="navbar-brand" href="/admin">Neighborhoods Admin</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/index.php">Dashboard<span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="/admin/books.php">Neighborhoods</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/construction.php">Users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/construction.php">Baskets</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/orders.php">Orders</a>
+                </li>
+
+                <li>
+                    <a class="nav-link red" href="/logout.php?logout=1">Logout</a>
+                </li>
+
+
+            </ul>
+        </div>
+    </div>
+</nav>
         <!-- end nav -->
         
         <!-- main content -->
         <div class="container-fluid">
             <h1>Honey We're Home Administration Page</h1>
-            <p>This is some text.</p>
+            <p>Select a neighborhood to update.</p>
             <table class="table table-striped">
                 <thead>
+
                     <tr>
                         <th>ID</th>
                         <th>Neighborhood</th>
                         <th>Location</th>
+                        <th>Community Centres</th>
                         <th>Description</th>
-                        <th>Rating</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach($neighborhood as $row) : ?>
+                    <?php foreach($result as $row) : ?>
                         <tr>
-                            <td>1</td>
-                            <td>Old Kildonan</td>
-                            <td>North</td>
-                            <td>Lorem Ipsum</td>
-                            <td>null</td>
+                            <td><?=$row['hood_id']?></td>
+                            <td><?=$row['name']?></a></td>
+                            <td><?=$row['location']?></td>
+                            <td><?=$row['comm_centres']?></td>
+                            <td><?=$row['description']?></td>
                             <td>
-                                <a class="btn btn-primary btn-sm" href="">edit</a>
+                                <a class="btn btn-primary btn-sm" href="/admin/neighborhood_edit.php">edit</a>
                                 <a class="delete btn btn-danger btn-sm" data_id="" href="#">delete</a>
                             </td>   
                         </tr>
