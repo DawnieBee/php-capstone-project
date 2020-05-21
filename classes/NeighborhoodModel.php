1ff61f3b4168a76018bdcc912080362e762dfa50
DawnBaker <deltabravo73@gmail.ca>
2020-05-20 23:01:04 -0500

    updated primary table NeighborhoodModel.php to update records.

--- classes/NeighborhoodModel.php
+++ classes/NeighborhoodModel.php
@@ -2,13 +2,35 @@
 
 namespace Capstone;
 
-
-
 class NeighborhoodModel extends Model
 {
     protected $table = 'neighborhoods';
     protected $key = 'hood_id';
 
+    public function updateNeighborhood()
+    {
+        $query = "UPDATE neighborhoods
+                SET 
+                name = :name,
+                location = :location,
+                rating_scale = :rating_scale,
+                description = :description
+                WHERE 
+                hood_id = :hood_id";
+
+    $stmt = $dbh->prepare($query);
+
+    $params = array(
+        ':name' => $_POST['name'],
+        ':location' => $_POST['location'],
+        ':rating_scale' => $_POST['rating_scale'],
+        ':description' => $_POST['description']
+    );
+
+    $stmt->execute($params);
+
+    header('Location: neighborhoods.php');
+    }
 
 }
 
