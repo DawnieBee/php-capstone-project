2f2ba299dadea90b8dca6ddd678258e89d976bfe
DawnBaker <deltabravo73@gmail.ca>
2020-05-21 14:26:34 -0500

    just updating the indenting of code

--- public/admin/neighborhoods.php
+++ public/admin/neighborhoods.php
@@ -20,90 +20,88 @@ $result = $neighborhood->all();
 
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Neighborhoods | Admin</title>
+    <title>Neighborhoods | Admin</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-    <div class="container">
-        <a class="navbar-brand" href="/admin">Neighborhoods Admin</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/admin/index.php">Home<span class="sr-only"></span></a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link active" href="/admin/neighborhoods.php">Neighborhoods</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/admin/users.php">Users</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/admin/baskets.php">Baskets</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/admin/orders.php">Orders</a>
-                </li>
-
-                <li>
-                    <a class="nav-link red" href="/logout.php?logout=1">Logout</a>
-                </li>
-
-
-            </ul>
-        </div>
-    </div>
-</nav>
-        <!-- end nav -->
-        
-        <!-- main content -->
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
         <div class="container">
-            <h1>Neighborhoods</h1>
-            
-            <table class="table table-striped">
-                <thead>
+            <a class="navbar-brand" href="/admin">Neighborhoods Admin</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/index.php">Home<span class="sr-only"></span></a>
+                    </li>
 
-                    <tr>
-                        <th>ID</th>
-                        <th>Neighborhood</th>
-                        <th>Location</th>
-                        <th>Rating</th>
-                        <th>Description</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($result as $row) : ?>
-                        <tr>
-                            <td><?=esc($row['hood_id'])?></td>
-                            <td><?=esc($row['name'])?></a></td>
-                            <td><?=esc($row['location'])?></td>
-                            <td><?=esc($row['rating_scale'])?></td>
-                            <td><?=esc($row['description'])?></td>
-                            <td>
-                                <a class="btn btn-primary btn-sm" href="neighborhood_edit.php?hood_id=<?=$row['hood_id']?>">edit</a>
-                                <a class="delete btn btn-danger btn-sm" data_id="" href="#">delete</a>
-                            </td>   
-                        </tr>
-                    <?php endforeach; ?>
-                    
-                </tbody>
-            </table>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/admin/neighborhoods.php">Neighborhoods</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/users.php">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/baskets.php">Baskets</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/orders.php">Orders</a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link red" href="/logout.php?logout=1">Logout</a>
+                    </li>
+
+
+                </ul>
+            </div>
         </div>
-    </body>
+    </nav> <!-- end nav -->
+        
+    <!-- main content -->
+    <div class="container">
+        <h1>Neighborhoods</h1>
+        
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Neighborhood</th>
+                    <th>Location</th>
+                    <th>Rating</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($result as $row) : ?>
+                    <tr>
+                        <td><?=esc($row['hood_id'])?></td>
+                        <td><?=esc($row['name'])?></a></td>
+                        <td><?=esc($row['location'])?></td>
+                        <td><?=esc($row['rating_scale'])?></td>
+                        <td><?=esc($row['description'])?></td>
+                        <td>
+                            <a class="btn btn-primary btn-sm" href="neighborhood_edit.php?hood_id=<?=$row['hood_id']?>">edit</a>
+                            <a class="delete btn btn-danger btn-sm" data_id="" href="#">delete</a>
+                        </td>   
+                    </tr>
+                <?php endforeach; ?>
+                
+            </tbody>
+        </table>
+    </div>
+</body>
 </html>
