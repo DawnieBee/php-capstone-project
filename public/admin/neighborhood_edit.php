ff472eebfdfd2aa80a7ecf7a338ae8626b44d249
DawnBaker <deltabravo73@gmail.ca>
2020-05-21 13:24:58 -0500

    updated resul to add the $_GET data retrieved from the button on neighborhoods.php

---
+++ public/admin/neighborhood_edit.php
@@ -0,0 +1,48 @@
+<?php
+
+require __DIR__ . '/../../config.php';
+require CLASSES . '/NeighborhoodModel.php';
+
+use Capstone\NeighborhoodModel;
+
+$neighborhood = new NeighborhoodModel();
+
+$result = $neighborhood->one($_GET['hood_id']);
+
+
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update List</title>
+</head>
+<body>
+
+
+    <h1>Update Neighborhoods</h1>
+    
+    <form action="neighborhood_update.php" method="post">
+        
+        <input type="hidden" name="hood_id" readonly value="<?=esc_attr(old('hood_id', $result))?>" />
+        <p><label for="name">Neighborhood: </label><br />
+            <input type="text" name="name" value="<?=esc_attr(old('name', $result))?>" /></p>
+        <p><label for="location">location: </label><br />
+            <input type="text" name="location" value="<?=esc_attr(old('location', $result))?>" /></p>
+        <p><label for="description">Description: </label><br />
+            <input type="text" name="description" value="<?=esc_attr(old('description', $result))?>" /></p>
+        <p><label for="rating_scale">Rating: </label><br />
+            <input type="text" name="rating_scale" value="<?=esc_attr(old('rating_scale', $result))?>" /></p>
+        <p><label for="police_station">Police Station: </label><br />
+            <input type="text" name="police_station" value="<?=esc_attr(old('police_station', $result))?>" /></p>
+
+
+
+
+        <p><button type="submit">Update Neighborhood</button></p>
+    </form>
+
+    
+</body>
+</html>
