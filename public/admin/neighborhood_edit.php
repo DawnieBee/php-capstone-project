95b31ce910e83ab0f6e80a63cf6133e997493270
DawnBaker <deltabravo73@gmail.ca>
2020-05-22 08:49:24 -0500

    updated the styling on neighborhood edit page and updated indenting

--- public/admin/neighborhood_edit.php
+++ public/admin/neighborhood_edit.php
@@ -25,16 +25,20 @@ if(!empty($post)){
 <html lang="en">
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Neighborhoods | Admin</title>
+    <title>Neighborhoods | Admin</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        label{
+            font-weight: 700;
+        }
+    </style>
 </head>
 
 <body>
@@ -69,47 +73,86 @@ if(!empty($post)){
             </div>
         </div>
     </nav> <!-- end nav -->
-    <h1>Update Neighborhoods</h1>
-    
-    <form action="neighborhood_update.php" method="post">
-        
-        <input type="hidden" name="hood_id" readonly value="<?=esc_attr(old('hood_id', $result))?>" />
-        <p><label for="name">Neighborhood: </label><br />
-            <input type="text" name="name" value="<?=esc_attr(old('name', $result))?>" /></p>
-        <p><label for="location">location: </label><br />
-            <input type="text" name="location" value="<?=esc_attr(old('location', $result))?>" /></p>
-        <p><label for="description">Description: </label><br />
-            <input type="text" name="description" value="<?=esc_attr(old('description', $result))?>" /></p>
-        <p><label for="img">Image: </label><br />
-            <input type="text" name="img" value="<?=esc_attr(old('img', $result))?>" /></p>
-        <p><label for="rating_scale">Rating: </label><br />
-            <input type="text" name="rating_scale" value="<?=esc_attr(old('rating_scale', $result))?>" /></p>
-        <p><label for="police_station">Police Station: </label><br />
-            <input type="text" name="police_station" value="<?=esc_attr(old('police_station', $result))?>" /></p>
-        <p><label for="fire_station">Fire Station: </label><br />
-            <input type="text" name="fire_station" value="<?=esc_attr(old('fire_station', $result))?>" /></p>
-        <p><label for="library">Library: </label><br />
-            <input type="text" name="library" value="<?=esc_attr(old('library', $result))?>" /></p>
-        <p><label for="pool">Pool: </label><br />
-            <input type="text" name="pool" value="<?=esc_attr(old('pool', $result))?>" /></p>
-        <p><label for="prim_schools">Primary Schools: </label><br />
-            <input type="text" name="prim_schools" value="<?=esc_attr(old('prim_schools', $result))?>" /></p>
-        <p><label for="sec_schools">Secondary Schools: </label><br />
-            <input type="text" name="sec_schools" value="<?=esc_attr(old('sec_schools', $result))?>" /></p>
-        <p><label for="churches">Churches: </label><br />
-            <input type="text" name="churches" value="<?=esc_attr(old('churches', $result))?>" /></p>
-        <p><label for="playgrounds">Playgrounds: </label><br />
-            <input type="text" name="playgrounds" value="<?=esc_attr(old('playgrounds', $result))?>" /></p>
-        <p><label for="comm_centres">Community Centres: </label><br />
-            <input type="text" name="comm_centres" value="<?=esc_attr(old('comm_centres', $result))?>" /></p>
-        <p><label for="house_price_min">House Price Minimum: </label><br />
-            <input type="text" name="house_price_min" value="<?=esc_attr(old('house_price_min', $result))?>" /></p>
-        <p><label for="house_price_max">House Price Maximum: </label><br />
-            <input type="text" name="house_price_max" value="<?=esc_attr(old('house_price_max', $result))?>" /></p>
-
-        <p><button type="submit">Update Neighborhood</button></p>
-    </form>
-
+    <!-- main content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Update Neighborhoods</h1>
+                
+                <form action="neighborhood_update.php" method="post">
+                    
+                    <input type="hidden" name="hood_id" readonly value="<?=esc_attr(old('hood_id', $result))?>" />
+                    <div class="form-group">
+                        <p><label for="name">Neighborhood: </label><br />
+                            <input class="form-control" type="text" name="name" value="<?=esc_attr(old('name', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="location">location: </label><br />
+                            <input class="form-control" type="text" name="location" value="<?=esc_attr(old('location', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="description">Description: </label><br />
+                            <input class="form-control" type="text" name="description" value="<?=esc_attr(old('description', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="img">Image: </label><br />
+                            <input class="form-control" type="text" name="img" value="<?=esc_attr(old('img', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="rating_scale">Rating: </label><br />
+                            <input class="form-control" type="text" name="rating_scale" value="<?=esc_attr(old('rating_scale', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="police_station">Police Station: </label><br />
+                            <input class="form-control" type="text" name="police_station" value="<?=esc_attr(old('police_station', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="fire_station">Fire Station: </label><br />
+                            <input class="form-control" type="text" name="fire_station" value="<?=esc_attr(old('fire_station', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="library">Library: </label><br />
+                            <input class="form-control" type="text" name="library" value="<?=esc_attr(old('library', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="pool">Pool: </label><br />
+                            <input class="form-control" type="text" name="pool" value="<?=esc_attr(old('pool', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="prim_schools">Primary Schools: </label><br />
+                            <input class="form-control" type="text" name="prim_schools" value="<?=esc_attr(old('prim_schools', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="sec_schools">Secondary Schools: </label><br />
+                            <input class="form-control" type="text" name="sec_schools" value="<?=esc_attr(old('sec_schools', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="churches">Churches: </label><br />
+                            <input class="form-control" type="text" name="churches" value="<?=esc_attr(old('churches', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="playgrounds">Playgrounds: </label><br />
+                            <input class="form-control" type="text" name="playgrounds" value="<?=esc_attr(old('playgrounds', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="comm_centres">Community Centres: </label><br />
+                            <input class="form-control" type="text" name="comm_centres" value="<?=esc_attr(old('comm_centres', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="house_price_min">House Price Minimum: </label><br />
+                            <input class="form-control" type="text" name="house_price_min" value="<?=esc_attr(old('house_price_min', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><label for="house_price_max">House Price Maximum: </label><br />
+                            <input class="form-control" type="text" name="house_price_max" value="<?=esc_attr(old('house_price_max', $result))?>" /></p>
+                    </div>
+                    <div class="form-group">
+                        <p><button type="submit" class="btn btn-primary">Update Neighborhood</button></p>
+                    </div>
+                </form>
+            </div> <!-- end col-->
+        </div><!-- end row -->
+    </div><!-- end container -->
     
 </body>
 </html>
