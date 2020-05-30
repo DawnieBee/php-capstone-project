2c5eeeacca86d0dc6eebb7c2c23d93a3895bb9f9
DawnBaker <deltabravo73@gmail.ca>
2020-05-30 10:27:39 -0500

    updated code on areas.php to make the page cleaner and less crowded  also added a back button on the area_detail.php

--- public/areas.php
+++ public/areas.php
@@ -23,14 +23,16 @@ $result = $neighborhood1->all();
           
           <?php foreach($result as $row) : ?>
             <h2><?=esc($row['name'])?></h2>
-              <div id="stjames" class="box">
-                <img src="images/<?=esc_attr($row['img'])?>" alt="<?=esc_attr($row['name'])?>"
-                     width="320" height="207" />
-                <div class="show_me curved_borders boxshadow">
-                  <?=esc($row['description'])?> 
-                </div>
-                <button id="areas_more"><a href="area_detail.php?hood_id=<?=$row['hood_id']?>">More</a></button>
+            <p class="item_head"><strong>Location: </strong><?=esc(ucwords($row['location']))?></p>
+            <div id="stjames" class="box">
+              <img src="images/<?=esc_attr($row['img'])?>" alt="<?=esc_attr($row['name'])?>"
+                   width="320" height="207" />
+              <div class="show_me curved_borders boxshadow">
+                <?=esc($row['description'])?> 
               </div>
+              
+            </div>
+            <button id="areas_more"><a href="area_detail.php?hood_id=<?=$row['hood_id']?>">More</a></button>
 
           <?php endforeach; ?>
         </div>
