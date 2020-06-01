a1578275f90f8372f1121844d7d52b0a714a5b56
DawnBaker <deltabravo73@gmail.ca>
2020-05-31 21:18:22 -0500

    updated BasketModel.php to what I THINK is right??

--- classes/BasketModel.php
+++ classes/BasketModel.php
@@ -6,4 +6,38 @@ class BasketModel extends Model
 {
     protected $table = 'baskets';
     protected $key = 'basket_id';
+
+    public function add($array)
+    {
+        $query = "INSERT INTO baskets
+            (
+                user_id,
+                first_name,
+                last_name,
+                email,
+
+            )
+            VALUES 
+            (
+                :user_id,
+                :first_name,
+                :last_name,
+                :email   
+            )";
+
+            $stmt = static::$dbh->prepare($query);
+
+            $params = array(
+                    ':user_id' => $array['user_id'],
+                    ':first_name' => $array['first_name'],,
+                    ':last_name' => $array['last_name'],,
+                    ':email' => $array['email']
+            );
+
+            $stmt->execute($params);
+
+            return static::$dbh->lastInsertId();
+    }
+
+    
 }
