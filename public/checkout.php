7566d94cd7f431beedf05751ed6e4c3ccfbbc3ad
DawnBaker <deltabravo73@gmail.ca>
2020-05-31 21:31:16 -0500

    finished checkout.php for the order to go to process page

---
+++ public/checkout.php
@@ -0,0 +1,62 @@
+<?php
+
+require __DIR__ . '/../config.php';
+
+
+$title = "Your Basket";
+
+// verify if there are hoods in the basket 
+if(count($_SESSION['basket']) == 0) {
+    $flash = array(
+        'class' => 'flash_error',
+        'message' => "There are no items in your basket."
+    );
+    $_SESSION['flash'] = $flash;
+    header('Location: neighborhoods.php');
+    die;
+}
+
+if(!auth()) {
+    $flash = array(
+        'class' => 'flash_error',
+        'message' => "You must be logged in to fill your order."
+    );
+    $_SESSION['flash'] = $flash;
+    $_SESSION['target'] = 'checkout.php';
+    header('Location: login.php');
+    die;
+}
+
+require __DIR__ . '/../includes/header.inc.php';
+?>
+
+    
+    <h2>Your basket of hoods</h2> 
+    
+
+    
+    <div>
+        <table class="basketofhoods">
+            <tr>
+                <th><strong>ID</strong></th>
+                <th><strong>Neighborhood</strong></th>
+                <th><strong>Location</strong></th>
+                <th><strong>Rating</strong></th>
+            </tr>
+            <tr>
+                <td><?=esc($_SESSION['basket']['hood_id'])?></td>
+                <td><?=esc($_SESSION['basket']['name'])?></td>
+                <td><?=esc($_SESSION['basket']['location'])?></td>
+                <td><?=esc($_SESSION['basket']['rating_scale'])?></td>  
+
+        </table>
+        <form action="process_order.php" method="post"></form>
+        <p><button type="submit">Confirm Order</button></p>
+    </div>
+
+    
+<?php
+
+require __DIR__ . '/../includes/footer.inc.php'
+
+?>
