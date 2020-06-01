56ebf8cf8213044273164183163a63b1a75f11bb
DawnBaker <deltabravo73@gmail.ca>
2020-05-31 21:29:49 -0500

    updated BasketModel.php AGAIN as was missing some fields

--- classes/BasketModel.php
+++ classes/BasketModel.php
@@ -12,26 +12,37 @@ class BasketModel extends Model
         $query = "INSERT INTO baskets
             (
                 user_id,
+                hood_id,
                 first_name,
                 last_name,
                 email,
-
+                name,
+                location,
+                rating_scale,
             )
             VALUES 
             (
                 :user_id,
+                :hood_id,
                 :first_name,
                 :last_name,
-                :email   
+                :email,
+                :name,
+                :location,
+                :rating_scale   
             )";
 
             $stmt = static::$dbh->prepare($query);
 
             $params = array(
                     ':user_id' => $array['user_id'],
-                    ':first_name' => $array['first_name'],,
-                    ':last_name' => $array['last_name'],,
-                    ':email' => $array['email']
+                    ':hood_id' => $array['hood_id'],
+                    ':first_name' => $array['first_name'],
+                    ':last_name' => $array['last_name'],
+                    ':email' => $array['email'],
+                    ':name' => $array['name'],
+                    ':location' => $array['location'],
+                    ':rating_scale' > $array['rating_scale']
             );
 
             $stmt->execute($params);
