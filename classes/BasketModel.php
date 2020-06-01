d34492b8a5ec8020d3a1df940b46ad191a8af9b3
DawnBaker <deltabravo73@gmail.ca>
2020-06-01 14:37:51 -0500

    updated BasketModel.php to be a mulit-item cart

--- classes/BasketModel.php
+++ classes/BasketModel.php
@@ -10,39 +10,17 @@ class BasketModel extends Model
     public function add($array)
     {
         $query = "INSERT INTO baskets
-            (
-                user_id,
-                hood_id,
-                first_name,
-                last_name,
-                email,
-                name,
-                location,
-                rating_scale,
-            )
-            VALUES 
-            (
-                :user_id,
-                :hood_id,
-                :first_name,
-                :last_name,
-                :email,
-                :name,
-                :location,
-                :rating_scale   
-            )";
+                (user_id, first_name, last_name, email)
+                VALUES 
+                (:user_id, :first_name, :last_name, :email)";
 
             $stmt = static::$dbh->prepare($query);
 
             $params = array(
                     ':user_id' => $array['user_id'],
-                    ':hood_id' => $array['hood_id'],
                     ':first_name' => $array['first_name'],
                     ':last_name' => $array['last_name'],
-                    ':email' => $array['email'],
-                    ':name' => $array['name'],
-                    ':location' => $array['location'],
-                    ':rating_scale' > $array['rating_scale']
+                    ':email' => $array['email']
             );
 
             $stmt->execute($params);
@@ -50,5 +28,46 @@ class BasketModel extends Model
             return static::$dbh->lastInsertId();
     }
 
+    public function addBasketItems($items, $basket_id)
+    {
+        $query = "INSERT INTO baskets_neighborhoods
+                (basket_id, hood_id, name, location, rating_scale)
+                VALUES
+                (:basket_id, :hood_id, :name, :location, :rating_scale)";
+
+        foreach($items as $item){
+            $stmt = static::$dbh->prepare($query);
+            $params = array(
+                ':basket_id' => $basket_id,
+                ':hood_id' => $item['hood_id'],
+                ':name' => $item['name'],
+                ':location' => $item['location'],
+                ':rating_scale' => $item['rating_scale']
+
+            );
+
+
+            $stmt->execute($params);
+        }
+
+    }
+
+    public function getBasketItems($basket_id)
+    {
+        $query = "SELECT * FROM baskets_neighborhoods
+                    WHERE basket_id = :basket_id";
+
+        $stmt = static::$dbh->prepare($query);
+        
+        $params = array(
+            ':basket_id' => intval($basket_id)
+        );
+
+        $stmt->execute($params);
+
+        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $result;
+    }
     
 }
