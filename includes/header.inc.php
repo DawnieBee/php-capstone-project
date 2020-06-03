28f8e3fe2c9b8aff58d652906ed6ce80ce57df97
DawnBaker <deltabravo73@gmail.ca>
2020-06-02 19:23:22 -0500

    added link to admin pages if logged in

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -328,6 +328,7 @@
                 <!--  if logged in show these  -->
                     <li class="curved_borders"><a <?=($title == "Profile") ? 'class="current"' : ''?> href="profile.php">Profile</a></li>
                     <li class="curved_borders"><a <?=($title == "Profile") ?> href="login.php?logout=1">Logout</a></li>
+                    <li class="curved_borders"><a <?=($title == "Profile") ?> href="/admin/index.php">Admin</a></li>
                 <?php else: ?>
                     <!-- if not logged in show these -->
                     <li class="curved_borders"><a <?=($title == "Register With Us") ? 'class="current"' : ''?> href="register.php">Sign Up</a></li>
