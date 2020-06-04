a23e6c92fb4ffb73412773d48d835f03456c8f74
DawnBaker <deltabravo73@gmail.ca>
2020-06-04 13:54:59 -0500

    instantiated the BasketModel and the UserModel to output info to the page.

--- public/profile.php
+++ public/profile.php
@@ -6,6 +6,12 @@
  * Capstone project
  */
 require __DIR__ . '/../config.php';
+require CLASSES . '/UserModel.php';
+require CLASSES . '/BasketModel.php';
+
+use Capstone\UserModel;
+use Capstone\BasketModel;
+
 
 // verify if there is a valid user logging in 
 if(empty($_SESSION['user_id'])) {
@@ -19,26 +25,12 @@ if(empty($_SESSION['user_id'])) {
     die;
 }
 // logged in user will get thier profile page and a message they are logged in 
-$query = 'SELECT * 
-        FROM users 
-        WHERE user_id = :user_id';
-$stmt = $dbh->prepare($query);
-$params = array(
-    ':user_id' => $_SESSION['user_id']
-    );
-$stmt->execute($params);
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+$model = new UserModel();
+$user = $model->userProfile($_SESSION['user_id']);
 
 // logged in user will get their basket info
-$query = "SELECT * 
-        FROM baskets
-        WHERE user_id = :user_id";
-$stmt = $dbh->prepare($query);
-$params = array(
-        'user_id' => $_SESSION['user_id']
-    );
-$stmt->execute($params);
-$basket = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$model = new BasketModel();
+$basket = $model->getUserBasket($_SESSION['basket_id']);
 
 $title = "Profile";
 
@@ -65,15 +57,17 @@ require __DIR__ . '/../includes/header.inc.php';
 
     <div class="basket_info">
         <h2>Your Baskets</h2>
+
         <?php foreach($basket as $row) : ?>
         <p>
             <strong>Basket</strong>: # <?=esc($row['basket_id'])?><br />
             <strong>Created</strong>: <?=esc($row['created_at'])?><br />
             <strong>Updated</strong>: <?=esc($row['updated_at'])?> </p>
-            <form action="/delete_bakset.php" method="post">
+            <form action="delete_basket.php" method="post">
                 <input type="hidden" name="csrf" value="<?=csrfToken()?>" />
                 <input type="hidden" name="basket_id" value="<?=esc_attr($row['basket_id'])?>" />
-                <button onclick="return (confirm('Are you sure you want to delete?'))" type="submit">Delete</button>
+                <button onclick="return (confirm('Are you sure you want to delete that basket?'))" type="submit">Delete</button>
+            </form>
             <hr />
         <?php endforeach; ?>
 
